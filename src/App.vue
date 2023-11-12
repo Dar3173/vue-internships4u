a82e6f1f3bb8b5362d3c679bef372ab7892e9da5
Darlyn <darlyn-1999@hotmail.com>
2023-11-12 17:05:06 -0500

    archivo base

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <FirstMenu />
 </template>
+
+<script setup>
+import FirstMenu from './components/FirstMenu.vue';
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  font-family: $roboto;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html, body{
+  margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	vertical-align: baseline;
+  background-image: url(./assets/patron.png);
+  background-repeat: repeat;
 }
 </style>
