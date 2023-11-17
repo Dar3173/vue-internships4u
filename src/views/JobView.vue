5921634da77882736006d7d79bf479788e1f173a
David-bx2 <josedavid-12@hotmail.com>
2023-11-17 14:48:06 -0600

    Creada la pagina JobView

---
+++ src/views/JobView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="post-container">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.content }}</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
