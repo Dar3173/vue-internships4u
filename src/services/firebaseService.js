539889d38cdc76a298bc7e3b229fb21440d2b1cb
David-bx2 <josedavid-12@hotmail.com>
2023-11-29 13:17:08 -0600

    Update firebaseService.js
    se comento el archivo firebaseservice.js para la tarea de estas semanas que piden el diseño y desarrollo de los servicios web

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, doc, getDoc, addDoc } from "firebase/firestore";
 
-
+// Configuración de Firebase con nuestras credenciales
 const firebaseConfig = {
     apiKey: "AIzaSyBq8Sl1VkCUAErrum7wQ1_BX15bakkO5QE",
     authDomain: "internships-4u-vue.firebaseapp.com",
@@ -12,17 +12,23 @@ const firebaseConfig = {
     appId: "1:970046382749:web:fafea2d721838538ee6a74"
 };
 
+// Inicialización de la aplicación Firebase
 const app = initializeApp(firebaseConfig);
+
+// Obtiene una instancia de Firestore
 const db = getFirestore(app);
+
+// Colección de posts en Firestore
 const postsCollection = collection(db, 'posts');
 
-
+// Función para obtener todos los posts desde la colección "posts"
 const fetchPosts = async () => {
     const querySnapshot = await getDocs(postsCollection);
     const posts = querySnapshot.docs.map(doc => ({ id: doc.id, data: doc.data() }));
     return posts;
 };
 
+// Función para obtener los detalles de un trabajo específico por su ID (postId)
 const fetchJobDetails = async (postId) => {
     const jobDocRef = doc(db, 'posts', postId);
     console.log(postId)
@@ -36,12 +42,15 @@ const fetchJobDetails = async (postId) => {
     }
 };
 
+// Colección de usuarios en Firestore
 const usersCollection = collection(db, 'users');
 
+
+// Función para agregar un nuevo usuario a la colección "users"
 const agregarUsuario = async (usuario) => {
-  // Agrega un nuevo usuario a la colección "users"
-  const docRef = await addDoc(usersCollection, usuario);
-  return docRef.id; // Devuelve el ID del documento recién creado
+    const docRef = await addDoc(usersCollection, usuario);
+    return docRef.id; // Devuelve el ID del documento recién creado
 };
 
+// Exporta las funciones para su uso en otros archivos
 export { fetchPosts, fetchJobDetails, agregarUsuario };
