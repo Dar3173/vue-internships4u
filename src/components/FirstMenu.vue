20f53e8ae9e6adbab1a349c0422d088e9b68d341
David-bx2 <josedavid-12@hotmail.com>
2023-12-03 17:42:32 -0600

    Update FirstMenu.vue
    se corrigio un error por el cual mostraba el nombre del usuario en paginas donde no deberia mostrarlo

--- src/components/FirstMenu.vue
+++ src/components/FirstMenu.vue
@@ -1,27 +1,43 @@
 <template>
     <div class="menu">
-        <img class="logo" src="../assets/logo.png" alt="logo Internships4u">
-        <template v-if="usuarioAutenticado">
+        <router-link :to="{ name: 'login' }">
+            <img class="logo" src="../assets/logo.png" alt="logo Internships4u">
+        </router-link>
+
+        <!-- Verifica si la ruta actual no está en la lista de rutas sin nombre de usuario -->
+        <template v-if="usuarioAutenticado && !esPaginaDeLoginYRegistro">
             <!-- Mostrar el nombre del usuario -->
             <span class="MenuLink">{{ nombreUsuario }}</span>
         </template>
+
+        <!-- Verifica si la ruta actual está en la lista de rutas sin nombre de usuario -->
+        <template v-else-if="esPaginaDeLoginYRegistro">
+            <!-- No mostrar nada en las páginas de login y registro -->
+        </template>
+
+        <!-- Verifica si la ruta actual no está en la lista de rutas sin nombre de usuario -->
         <template v-else>
             <!-- Mostrar el enlace de "Iniciar Sesión" si el usuario no está autenticado -->
             <span class="MenuLink">Iniciar Sesión</span>
         </template>
     </div>
 </template>
-  
+
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch, getCurrentInstance } from 'vue';
 import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const usuarioAutenticado = ref(false);
 const nombreUsuario = ref('');
 const auth = getAuth();
+const vm = getCurrentInstance();
 
+// Rutas donde no quieres mostrar el nombre del usuario
+const rutasSinNombreUsuario = ['login', 'Signup'];
 
+// Obtener la ruta actual
+const esPaginaDeLoginYRegistro = ref(false);
 
 onMounted(async () => {
     onAuthStateChanged(auth, async (user) => {
@@ -36,7 +52,6 @@ onMounted(async () => {
             try {
                 const userSnapshot = await getDoc(userDoc);
 
-                // Mueve la impresión del log después de verificar si existe el documento
                 if (userSnapshot.exists()) {
                     const userData = userSnapshot.data();
                     nombreUsuario.value = userData.nombre;
@@ -50,6 +65,15 @@ onMounted(async () => {
             // Usuario no autenticado
             usuarioAutenticado.value = false;
         }
+        // Verificar si la ruta actual está en la lista de rutas sin nombre de usuario
+        const rutaActual = vm.proxy.$route.name;
+        esPaginaDeLoginYRegistro.value = rutasSinNombreUsuario.includes(rutaActual);
+    });
+
+    // Observar cambios en la ruta
+    watch(() => vm.proxy.$route, (to) => {
+        const rutaActual = to.name;
+        esPaginaDeLoginYRegistro.value = rutasSinNombreUsuario.includes(rutaActual);
     });
 });
 </script>
